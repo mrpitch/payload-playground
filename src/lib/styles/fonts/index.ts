f6355d2109b843c8dbeffec16b2f4f0aac26bec8
mrpitch <mrpitch@outlook.com>
2025-02-01 15:37:17 +0100

    feat: add custom logo, icon and tailwindcss support for payload ui

--- src/lib/styles/fonts/index.ts
+++ src/lib/styles/fonts/index.ts
@@ -10,24 +10,24 @@ const {
 */
 
 export const typeNextLight: NextFontWithVariable = localFont({
-	src: 'inter-latin-200.woff2',
+	src: './inter-latin-200.woff2',
 	display: 'swap',
 	variable: '--font-typeNextLight',
 })
 
 export const typeNextRegular: NextFontWithVariable = localFont({
-	src: 'inter-latin-regular.woff2',
+	src: './inter-latin-regular.woff2',
 	display: 'swap',
 	variable: '--font-typeNextRegular',
 })
 
 export const typeNextSemiBold: NextFontWithVariable = localFont({
-	src: 'inter-latin-600.woff2',
+	src: './inter-latin-600.woff2',
 	display: 'swap',
 	variable: '--font-typeNextSemiBold',
 })
 export const typeNextBold: NextFontWithVariable = localFont({
-	src: 'inter-latin-800.woff2',
+	src: './inter-latin-800.woff2',
 	display: 'swap',
 	variable: '--font-typeNextBold',
 })
--- src/payload/styles/Icon.tsx
+++ src/payload/styles/Icon.tsx
@@ -1,33 +1,17 @@
-const css = `
-  html[data-theme="dark"] .text {
-    fill: #0C0C0C;
-  }
-
-  html[data-theme="dark"] .bg {
-    fill: white;
-  }
-
-  .graphic-icon {
-    width: 25px;
-    height: 25px;
-  }
-`
-
 const Icon = () => {
-	// const baseStyles = {
-	// 	default: 'w-11 h-11 bg-primary rounded-full p-0.5',
-	// }
+	const baseStyles = {
+		default: 'w-6 h-5',
+	}
 
 	return (
 		<svg
-			className="graphic-icon"
+			className={baseStyles.default}
 			width="100%"
 			height="100%"
 			viewBox="0 0 82 100"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
-			<style>{css}</style>
 			<path
 				d="M45.4 19.5V7H6V92.8824H45.4V80.3824C37.3289 80.3824 29.5882 77.175 23.8812 71.4662C18.1741 65.7574 14.9679 58.0147 14.9679 49.9412C14.9679 41.8677 18.1741 34.1247 23.8812 28.416C29.5882 22.7072 37.3289 19.5 45.4 19.5Z"
 				fill="currentColor"
