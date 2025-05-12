b558f071f4039163e87bec36fba5124054c7609a
mrpitch <mrpitch@outlook.com>
2025-05-12 17:47:02 +0200

    fix(seed): correct image path in seeding script
    - Update image source path in the seed script to ensure it points to the correct location.
    - Verify that the seeding process references the appropriate image paths for successful execution.

---
+++ src/app/(app)/error.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { startTransition } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
+import { FormContainer } from '@/components/auth/form-container'
+import { Button, buttonVariants } from '@/components/ui/button'
+export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+	const router = useRouter()
+
+	const handleRetry = () => {
+		startTransition(() => {
+			router.refresh()
+			reset()
+		})
+	}
+	const { message } = error
+	console.log('message', message)
+	return (
+		<FormContainer title={message}>
+			<div className="flex flex-col gap-4">
+				<Button onClick={handleRetry} variant="secondary">
+					Try again
+				</Button>
+			</div>
+		</FormContainer>
+	)
+}
